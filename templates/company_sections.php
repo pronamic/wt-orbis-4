3e3f20fa382b31de11e60e56dc886cdc73b543d0
Remco Tolsma <869674+remcotolsma@users.noreply.github.com>
2023-02-21 14:18:10 +0100

    Fix https://github.com/pronamic/orbis.pronamic.nl/issues/7.
    https://getbootstrap.com/docs/5.0/components/navs-tabs/#javascript-behavior

--- templates/company_sections.php
+++ templates/company_sections.php
@@ -6,13 +6,37 @@ if ( ! empty( $sections ) ) : ?>
 
 	<div class="card mb-3 with-cols clearfix">
 		<div class="card-header">
-			<ul class="nav nav-tabs card-header-tabs" id="company-tabs">
+			<ul class="nav nav-tabs card-header-tabs" id="company-tabs" role="tablist">
 				<?php $active = true; ?>
 
 				<?php foreach ( $sections as $section ) : ?>
 
-					<li class="nav-item">
-						<a href="#<?php echo esc_attr( $section['id'] ); ?>" class="nav-link <?php echo esc_attr( $active ? 'active' : '' ); ?>" data-toggle="tab"><?php echo esc_html( $section['name'] ); ?></a>
+					<li class="nav-item" role="presentation">
+						<?php
+
+						$id    = $section['id'];
+						$label = $section['name'];
+
+						$classes = [
+							'nav-link',
+						];
+
+						if ( $active ) {
+							$classes[] = 'active';
+						}
+
+						$button_id = $id . '-tab';
+
+						printf(
+							'<button class="%s" id="%s" data-bs-toggle="tab" data-bs-target="%s" type="button" role="tab" aria-controls="%s" aria-selected="true">%s</button>',
+							\esc_attr( implode( ' ', $classes ) ),
+							\esc_attr( $button_id ),
+							\esc_attr( '#' . $id ),
+							\esc_attr( $id ),
+							\esc_html( $label )
+						);
+
+						?>
 					</li>
 
 					<?php $active = false; ?>
@@ -26,7 +50,27 @@ if ( ! empty( $sections ) ) : ?>
 
 			<?php foreach ( $sections as $section ) : ?>
 
-				<div id="<?php echo esc_attr( $section['id'] ); ?>" class="tab-pane <?php echo esc_attr( $active ? 'active' : '' ); ?>">
+				<?php
+
+				$id = $section['id'];
+
+				$button_id = $id . '-tab';
+
+				$classes = [
+					'tab-pane',
+					'fade',
+				];
+
+				if ( $active ) {
+					$classes[] = 'show';
+					$classes[] = 'active';
+				}
+
+				$class = implode( ' ', $classes );
+
+				?>
+
+				<div id="<?php echo esc_attr( $id ); ?>" class="<?php echo esc_attr( $class ); ?>" role="tabpanel" aria-labelledby="<?php echo esc_attr( $button_id ); ?>">
 					<?php
 
 					if ( isset( $section['action'] ) ) {
